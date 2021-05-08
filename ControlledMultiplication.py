257dbf11888f8b9cb11053a898d4e0cae6f7b511
Kamiel Fokkink <kamielfokkink@gmail.com>
2021-05-08 21:10:24 +0200

    Implement first version of Shor's algorithm

---
+++ ControlledMultiplication.py
@@ -0,0 +1,31 @@
+#file: ControlledMultiplication.py
+#authors: Kamiel Fokkink, Bram Mak
+
+#This code implements a controlled multiplication gate, of mod 15
+
+from qiskit import QuantumCircuit
+
+def c_amod15(a, power):
+    """Controlled multiplication by a mod 15"""
+    if a not in [2,7,8,11,13]:
+        raise ValueError("'a' must be 2,7,8,11 or 13")
+    U = QuantumCircuit(4)        
+    for iteration in range(power):
+        if a in [2,13]:
+            U.swap(0,1)
+            U.swap(1,2)
+            U.swap(2,3)
+        if a in [7,8]:
+            U.swap(2,3)
+            U.swap(1,2)
+            U.swap(0,1)
+        if a == 11:
+            U.swap(1,3)
+            U.swap(0,2)
+        if a in [7,11,13]:
+            for q in range(4):
+                U.x(q)
+    U = U.to_gate()
+    U.name = "%i^%i mod 15" % (a, power)
+    c_U = U.control()
+    return c_U
--- QuantumFourierTransform.py
+++ QuantumFourierTransform.py
@@ -1,23 +1,20 @@
 #file: QuantumFourierTransform.py
 #authors: Kamiel Fokkink, Bram Mak
 
-#This code implements the quantum fourier transform on a given list of qubits
-
-import cirq
+#This code implements the quantum fourier transform on a given on a circuit
+#with amount of qubits given by qubits
+    
+import qiskit as qs
+import numpy as np
 
 def QFT(qubits):
-    circuit = cirq.Circuit()
-
-    n = len(qubits)
-    for i, q in enumerate(qubits):
-        circuit.append(cirq.H(q))
-        for i2 in range(1,n-i):
-            circuit.append((cirq.S(q)**(2**-(i2-1))).controlled_by(qubits[i2+i]))
-    if (len(qubits)%2==0):
-        for i in range(int(len(qubits)/2)):
-            circuit.append(cirq.SWAP(qubits[i],qubits[-i-1]))
-    else:
-        for i in range(int(float(len(qubits)/2) - 0.5)):
-            circuit.append(cirq.SWAP(qubits[i],qubits[-i-1]))
-
+    circuit = qs.QuantumCircuit(qubits)
+    
+    for qubit in range(qubits//2):
+        circuit.swap(qubit,qubits-qubit-1)
+    for j in range(qubits):
+        for m in range(j):
+            circuit.cp(-np.pi/float(2**(j-m)), m, j)
+        circuit.h(j)
+        
     return circuit
