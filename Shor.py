095e57f4d630383ec3db958e89134e677cdd5cde
qwerty514 <acmbasketbal@gmail.com>
2021-05-12 12:46:38 +0200

    Modularized Shor.py Set up main.py to execute Shor circuit on ibmq

--- Shor.py
+++ Shor.py
@@ -10,49 +10,54 @@ from qiskit.visualization import plot_histogram
 from fractions import Fraction
 import pandas as pd
 
-n_count = 8  # number of counting qubits
-a = 7
+def ShorCircuit():
+    n_count = 8  # number of counting qubits
+    a = 7
 
-qc = QuantumCircuit(n_count + 4, n_count)
+    qc = QuantumCircuit(n_count + 4, n_count)
 
-# Initialise counting qubits
-# in state |+>
-for q in range(n_count):
-    qc.h(q)
+    # Initialise counting qubits
+    # in state |+>
+    for q in range(n_count):
+        qc.h(q)
 
-# And auxiliary register in state |1>
-qc.x(3+n_count)
+    # And auxiliary register in state |1>
+    qc.x(3+n_count)
 
-# Do controlled-U operations
-for q in range(n_count):
-    qc.append(c_amod15(a, 2**q),
-             [q] + [i+n_count for i in range(4)])
+    # Do controlled-U operations
+    for q in range(n_count):
+        qc.append(c_amod15(a, 2**q),
+                 [q] + [i+n_count for i in range(4)])
 
-# Do inverse-QFT
-qc.append(QFT(n_count), range(n_count))
+    # Do inverse-QFT
+    qc.append(QFT(n_count), range(n_count))
 
-# Measure circuit
-qc.measure(range(n_count), range(n_count))
-qc.draw(fold=-1)  # -1 means 'do not fold'
+    # Measure circuit
+    qc.measure(range(n_count), range(n_count))
+    qc.draw(fold=-1)  # -1 means 'do not fold'
 
-qasm_sim = Aer.get_backend('qasm_simulator')
-t_qc = transpile(qc, qasm_sim)
-qobj = assemble(t_qc)
-results = qasm_sim.run(qobj).result()
-counts = results.get_counts()
+    return qc
 
 #plot_histogram(counts) Do this to see a histogram of the counts
 
-measured_phases = []
-for output in counts:
-    decimal = int(output, 2)  # Convert (base 2) string to decimal
-    phase = decimal/(2**n_count)  # Find corresponding eigenvalue
-    measured_phases.append(phase)
+def PostProcess(resultCounts):
+    n_count = 8  # number of counting qubits
+    measured_phases = []
+    for output in resultCounts:
+        decimal = int(output, 2)  # Convert (base 2) string to decimal
+        phase = decimal/(2**n_count)  # Find corresponding eigenvalue
+        measured_phases.append(phase)
 
-rows = []
-for phase in measured_phases:
-    frac = Fraction(phase).limit_denominator(15)
-    rows.append([phase, f"{frac.numerator}/{frac.denominator}", frac.denominator])
-headers=["Phase", "Fraction", "Guess for r"]
-df = pd.DataFrame(rows, columns=headers)
-#print(df) to see the final results
+    rows = []
+    for phase in measured_phases:
+        frac = Fraction(phase).limit_denominator(15)
+        rows.append([phase, f"{frac.numerator}/{frac.denominator}", frac.denominator])
+    headers=["Phase", "Fraction", "Guess for r"]
+    df = pd.DataFrame(rows, columns=headers)
+    #print(df) to see the final results
+
+qasm_sim = Aer.get_backend('qasm_simulator')
+t_qc = transpile(ShorCircuit(), qasm_sim)
+qobj = assemble(t_qc)
+results = qasm_sim.run(qobj).result()
+counts = results.get_counts()
--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from credi import GetCredi
+from Shor import ShorCircuit
 from qiskit import IBMQ, QuantumCircuit, assemble, transpile
 
 provider = IBMQ.enable_account(GetCredi())  #API Token in file ignored by git
-backend = provider.get_backend("ibmq_santiago")
+backend = provider.get_backend("ibmq_16_melbourne") #only melbourne fits the circuit
 
-testcircuit = QuantumCircuit(4)
-testcircuit.h(0)
-testcircuit.cx(0, 1)
-testcircuit.cx(0, 2)
-testcircuit.x(3)
+testcircuit = ShorCircuit()
 testcircuit = transpile(testcircuit, backend)
 
 job = backend.run(testcircuit)
+print(job.job_id)
+
 print(job.result())
