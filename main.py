a8ef2207fb59fb7694239bb177fd6c27e83ff8d4
qwerty514 <acmbasketbal@gmail.com>
2021-05-12 13:14:33 +0200

    Refined the framework for testing

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ from Shor import ShorCircuit, PostProcess
 from qiskit import IBMQ, QuantumCircuit, assemble, transpile, Aer
 from qiskit.visualization import plot_histogram
 
-def Simulate():
+def Simulate(a=7):
     qasm_sim = Aer.get_backend('qasm_simulator')
-    t_qc = transpile(ShorCircuit(), qasm_sim)
+    t_qc = transpile(ShorCircuit(a=7), qasm_sim)
     qobj = assemble(t_qc)
     results = qasm_sim.run(qobj).result()
     PostProcess(results.get_counts())
 
-def Excecute():
+# Take an a, generate circuit and run this on IBMQ
+# Job ID of the job on IBMQ
+def Excecute(a):
     provider = IBMQ.enable_account(GetCredi())  #API Token in file ignored by git
     backend = provider.get_backend("ibmq_16_melbourne") #only melbourne fits the circuit
 
-    testcircuit = ShorCircuit()
+    testcircuit = ShorCircuit(a)
     testcircuit = transpile(testcircuit, backend)
 
     job = backend.run(testcircuit)
@@ -22,6 +24,7 @@ def Excecute():
     return job.job_id
 
 
+# Take a job_id, get the results from IBMQ
+# Returns results of the job!
 def PostExcecute(jobID):
     print(job.result())
-    PostProcess(job.result.get_counts())
