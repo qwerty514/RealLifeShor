e0730c7177520f2461333a1fca882a676ad58b33
qwerty514 <acmbasketbal@gmail.com>
2021-05-18 11:43:52 +0200

    Added RetrieveAll Minor changes

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ backend = None
 # Simulate a Shor circuit for a given a, return results immediately
 def Simulate(a=7):
     qasm_sim = Aer.get_backend('qasm_simulator')
-    t_qc = transpile(ShorCircuit(a=7), qasm_sim)
+    t_qc = transpile(ShorCircuit(a), qasm_sim)
     qobj = assemble(t_qc)
     return qasm_sim.run(qobj).result()
 
@@ -29,21 +29,32 @@ def Excecute(a=7):
     return job.job_id
 
 def ExecuteAll(n):
-    guesses = [2,7,8,11,13]
+    guesses = [2, 7, 8, 11, 13]
     records = []
     for a in guesses:
         for _ in range(3):
             jobID = Excecute(a)
-            records.append([a,jobID])
+            records.append([a, jobID])
     
-    df = pd.DataFrame(records,columns=["a","jobID"])
+    df = pd.DataFrame(records, columns=["a", "jobID"])
     path = "JobIDs/record" + str(n) + ".csv"
-    df.to_csv(path,ignore_index=True)
+    df.to_csv(path, ignore_index=True)
 
 # Take a job_id, get the results from IBMQ
 # Returns results of the job!
-def PostExcecute(jobID):
-    if PostExcecute.backend is None:
-        PostExcecute.provider = IBMQ.enable_account(GetCredi())  #API Token in file ignored by git
-        PostExcecute.backend = PostExcecute.get_backend("ibmq_16_melbourne") #only melbourne fits the circuit
-    return PostExcecute.backend.retrieve_job(jobID).result()
+def RetrieveResult(jobID):
+    if RetrieveResult.backend is None:
+        RetrieveResult.provider = IBMQ.enable_account(GetCredi())  #API Token in file ignored by git
+        RetrieveResult.backend = RetrieveResult.get_backend("ibmq_16_melbourne") #only melbourne fits the circuit
+    return RetrieveResult.backend.retrieve_job(jobID).result()
+
+def RetrieveAll(n):
+    path = "JobIDs/record" + str(n) + ".csv"
+    df = pd.read_csv(path)
+    for ID in df["jobID"]:
+        print(df[ID][1])
+        print(df[ID][0])
+        print(RetrieveResult(ID))
+
+
+
