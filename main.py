6781dc26e0df98d0ff23097560936d79531f82a1
qwerty514 <acmbasketbal@gmail.com>
2021-05-27 14:13:47 +0200

    Using exception handling, I hope this fixes it now

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from credi import GetCredi
 from Shor import ShorCircuit, PostProcess
 from visualize import makeBarPlots, guessFactors
 from qiskit import IBMQ, QuantumCircuit, assemble, transpile, Aer, execute
+from qiskit.providers.ibmq.exceptions import IBMQAccountError
 from qiskit.visualization import plot_histogram
 from qiskit.providers.aer import AerSimulator
 from qiskit.providers.aer.noise import NoiseModel
@@ -10,25 +11,31 @@ import pandas as pd
 from pathlib import Path
 
 # Simulate a Shor circuit for a given a, return results immediately
-def Simulate(a=7, model=None):
+def Simulate(a = 7, model=None):
     if model is not None:
-        if Execute.provider is not None:
+        try:
+            backup = Execute.provider
             Execute.provider = IBMQ.enable_account(GetCredi())  # API Token in file ignored by git
+        except IBMQAccountError as e:
+            Execute.provider = backup
+            pass
+
         Execute.backend = Execute.provider.get_backend(model)  # only melbourne fits the circuit
         noiseModel = NoiseModel.from_backend(Execute.backend)
-        #simul = AerSimulator(noise_model=noiseModel)
+        # simul = AerSimulator(noise_model=noiseModel)
         coupling = Execute.backend.configuration().coupling_map
         basisGates = noiseModel.basis_gates
         return execute(ShorCircuit(a), Aer.get_backend('qasm_simulator'),
-                        coupling_map=coupling,
-                        basis_gates=basisGates,
-                        noise_model=noiseModel).result()
-        #return simul.run(ShorCircuit(a)).result()
+                       coupling_map=coupling,
+                       basis_gates=basisGates,
+                       noise_model=noiseModel).result()
+        # return simul.run(ShorCircuit(a)).result()
     else:
         qasm_sim = Aer.get_backend('qasm_simulator')
         t_qc = transpile(ShorCircuit(a), qasm_sim)
         qobj = assemble(t_qc)
         return qasm_sim.run(qobj).result()
+
 
 # Run Shor circuit for multiple guesses of a, and save the results as bar charts
 # and tables
@@ -43,10 +50,11 @@ def SimulateAll():
         df2, counts2 = PostProcess(Simulate(a, model="ibmq_16_melbourne"), a)
         results2[a] = counts2
         guessFactors(df2, a, 15, "NoiseModel")
-        #PostProcess(Simulate(a, "ibmq_16_melbourne"), a)
-        #PostProcess(Simulate(a, "qasm_simulator"), a)
+        # PostProcess(Simulate(a, "ibmq_16_melbourne"), a)
+        # PostProcess(Simulate(a, "qasm_simulator"), a)
     makeBarPlots(results1, results2)
-    #return results1,results2
+    # return results1,results2
+
 
 # Take an a, generate circuit and run this on IBMQ
 # Job ID of the job on IBMQ
@@ -61,6 +69,7 @@ def Execute(a=7):
     job = Execute.backend.run(testcircuit)
     print(job.job_id)
     return job.job_id
+
 
 def ExecuteAll(n, a, amount):
     if a not in aArray:
@@ -95,6 +104,7 @@ def RetrieveAll(n):
         print(df[ID][0])
         print(RetrieveResult(ID))
 
+
 Execute.provider = None
 Execute.backend = None
 
@@ -102,4 +112,4 @@ aArray = [2, 7, 8, 11, 13]
 
 SimulateAll()
 
-#Simulate(7,"ibmq_16_melbourne")
+# Simulate(7,"ibmq_16_melbourne")
